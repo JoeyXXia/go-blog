764604ef8dca684f4d7675d896919360cf52d392
Xu <whitenight.xiaxu@gmail.com>
2025-05-13 15:02:25 +0800

    config project

---
+++ web/go-blog/src/router/index.ts
@@ -0,0 +1,23 @@
+import DashBoard from '@/views/dashboard/DashBoard.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: DashBoard,
+    },
+    {
+      path: '/users',
+      name: 'users',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/views/users/UsersInfo.vue'),
+    },
+  ],
+})
+
+export default router

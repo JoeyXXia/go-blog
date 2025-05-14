16f6053f3828ff65a2cef5ca55d9f1b8fb299793
Xu <whitenight.xiaxu@gmail.com>
2025-05-14 15:21:56 +0800

    Update index.ts

---
+++ web/go-blog/src/services/index.ts
@@ -0,0 +1,10 @@
+import { BASE_URL, TIME_OUT } from './config'
+import XXRequest from './request'
+
+const xxRequest = new XXRequest({
+  baseURL: BASE_URL,
+  timeout: TIME_OUT,
+  interceptors: {},
+})
+
+export default xxRequest

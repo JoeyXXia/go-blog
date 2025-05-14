e9664d6f5ebffa9328e540ac0eec67f2309385d5
Xu <whitenight.xiaxu@gmail.com>
2025-05-14 14:37:10 +0800

    register icons

--- web/go-blog/src/main.ts
+++ web/go-blog/src/main.ts
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-
 import 'normalize.css'
 import './assets/css/index.less'
 
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import registerIcons from '@/utils/register-icons'
 
 const app = createApp(App)
-
+app.use(registerIcons)
 app.use(createPinia())
 app.use(router)
 
--- web/go-blog/src/views/dashboard/DashBoard.vue
+++ web/go-blog/src/views/dashboard/DashBoard.vue
@@ -2,6 +2,15 @@
 
 <template>
   <div class="dashboard">dashboard</div>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+  <el-icon><Unlock /></el-icon>
 </template>
 
 <style lang="less" scoped></style>
